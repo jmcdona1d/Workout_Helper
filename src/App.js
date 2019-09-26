d2539d3e88e38035203f49c99d1d3332b5e3879a
James McDonald <15jgm4@queensu.ca>
2019-09-26 11:52:22 -0400

    added react app base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import NameForm from "./components/Weight_entry";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Main Page">
+      <NameForm />
+    </div>
+  );
+}
+
+export default App;
