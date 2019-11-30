7f396742f1fda5026fe0215198d2331e4334c1de
James McDonald <15jgm4@queensu.ca>
2019-11-30 16:22:07 -0500

    Updates

---
+++ precache-manifest.4effc87e1ef5f142b93e7d9af0da82e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab5fc128125363c415e83735d06ad5b",
+    "url": "/Workout_Helper/index.html"
+  },
+  {
+    "revision": "8f8d57e7d2a5a5ad95c2",
+    "url": "/Workout_Helper/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7ddf415160bfabe03646",
+    "url": "/Workout_Helper/static/css/main.5efa301c.chunk.css"
+  },
+  {
+    "revision": "8f8d57e7d2a5a5ad95c2",
+    "url": "/Workout_Helper/static/js/2.65c62cfc.chunk.js"
+  },
+  {
+    "revision": "7ddf415160bfabe03646",
+    "url": "/Workout_Helper/static/js/main.cbdb3752.chunk.js"
+  },
+  {
+    "revision": "65c2abfc40205e293def",
+    "url": "/Workout_Helper/static/js/runtime~main.d887d386.js"
+  },
+  {
+    "revision": "262932124a19a3ff467e7f7045763b66",
+    "url": "/Workout_Helper/static/media/Deadlift_pic.26293212.jpg"
+  },
+  {
+    "revision": "c12076b2927dcbd64763a95c3cc74a53",
+    "url": "/Workout_Helper/static/media/pattern_background.c12076b2.png"
+  }
+]);
