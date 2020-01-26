87edd19f9bb74ae566b2b5ae9d44cea3184ffd61
James McDonald <15jgm4@queensu.ca>
2020-01-25 19:51:40 -0500

    Set up server backend

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+//BodyParser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("mongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+app.use("/api/users", users);
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
